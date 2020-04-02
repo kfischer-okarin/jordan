723665c234a2635fec776824765656974cd80bb5
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-04-02 23:02:51 +0900

    Fix credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   def sign_in
     payload = { exp: 24.hours.from_now.to_i, user_id: id }
-    JWT.encode(payload, Rails.application.secrets.secret_key_base).tap { |token|
+    JWT.encode(payload, Rails.application.credentials.secret_key_base).tap { |token|
       update(token: token)
     }
   end
 
   def self.from_token(token)
-    payload, _ = JWT.decode(token, Rails.application.secrets.secret_key_base)
+    payload, _ = JWT.decode(token, Rails.application.credentials.secret_key_base)
     find(payload['user_id'])
   end
 end
